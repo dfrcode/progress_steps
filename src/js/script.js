9aa338eb430582b9965d18cef5dba92f16ae5bf1
Denis Frolov <denisfrolov.coder@gmail.com>
2021-03-18 20:24:04 +0300

    deploy on github pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,41 @@
-const prev = document.querySelector('.prev_btn')
-const next = document.querySelector('.next_btn')
+import "../css/style.scss";
 
-const listItems = document.querySelectorAll('.progress_steps_box_list_item')
-const len = listItems.length
+const progress = document.getElementById("progress"),
+  prev = document.getElementById("prev"),
+  next = document.getElementById("next"),
+  circles = document.querySelectorAll(".container_progress_circle");
 
-prev.disabled = true
+let currentActive = 1;
 
-let i = 0;
-next.addEventListener('click', () => {
-    if (i !== len - 1) {
-        prev.disabled = false
-        console.log(i++)
-    } else {
-        next.disabled = true
-    }
-})
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length - 1) {
+    currentActive = circles.length;
+    next.disabled = true;
+  }
+  prev.disabled = false;
+  addActiveClass()
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 2) {
+    currentActive = 1;
+    prev.disabled = true;
+  }
+  next.disabled = false;
+  addActiveClass()
+});
+
+function addActiveClass() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+    const actives = document.querySelectorAll('.active')
+    progress.style.width = `${(actives.length - 1) / (circles.length - 1) * 100}%`
+    console.log(progress.style.width)
+} 
